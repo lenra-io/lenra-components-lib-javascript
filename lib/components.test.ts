22c02188e89d8b2a246e86c5292305e2a3ab07c1
Thomas DA ROCHA <thomas.darocha@lenra.io>
2023-09-14 18:08:06 +0200

    test: Fix test

--- lib/components.test.ts
+++ lib/components.test.ts
@@ -2,42 +2,40 @@ import * as components from "./components";
 import * as path from "path";
 import * as fs from "fs";
 
-const API_PATH = path.resolve(__dirname, "..", "components-api", "api");
-const COMPONENTS_PATH = path.join(API_PATH, "components");
+const apiVersion = fs.readFileSync("api-version.txt", "utf-8").trim();
+const schemaPath = path.resolve("schemata", `${apiVersion}.json`);
 
-test.skip("components-api loaded", () => {
-  expect(fs.existsSync(API_PATH)).toBe(true);
-  expect(fs.existsSync(COMPONENTS_PATH)).toBe(true);
+test("lenra-components API loaded", () => {
+  expect(fs.existsSync(schemaPath)).toBe(true);
 });
 
-// describe("Managed component", () => {
-//   const componentsSchema = fs.readdirSync(COMPONENTS_PATH);
+describe("Managed component", () => {
+  const componentsSchema = JSON.parse(fs.readFileSync(schemaPath, "utf-8"));
 
-//   describe.each(componentsSchema.map((comp) => [comp]))("%s", (comp) => {
-//     const content = JSON.parse(
-//       fs.readFileSync(path.join(COMPONENTS_PATH, comp), "utf-8")
-//     );
-//     const f = components[content.title];
-//     const c = components[`${content.title}Impl`];
-//     test(`function ${content.title} exists`, () => {
-//       expect(f).toBeDefined();
-//     });
-//     test(`class ${content.title}Impl exists`, () => {
-//       expect(c).toBeDefined();
-//     });
+  describe.each(componentsSchema.oneOf.map((c: { $ref: string }) => c.$ref))("%s", (ref) => {
+    // Check if the class exists
+    const content = ref.replace(/^#\/?/, "").split("/").reduce((o, part) => o[part], componentsSchema);
+    const f = components[content.title];
+    const c = components[`${content.title}Impl`];
+    test(`function ${content.title} exists`, () => {
+      expect(f).toBeDefined();
+    });
+    test(`class ${content.title}Impl exists`, () => {
+      expect(c).toBeDefined();
+    });
 
-//     if (c) {
-//       describe("methods", () => {
-//         const properties = Object.entries(content.properties).filter(
-//           ([key, _]) => !(content.required || []).includes(key)
-//         );
+    if (c) {
+      describe("methods", () => {
+        const properties = Object.entries(content.properties).filter(
+          ([key, _]) => !(content.required || []).includes(key)
+        );
 
-//         if (properties.length > 0) {
-//           test.each(properties)(`%s exists`, (key, descriptor) => {
-//             expect(c.prototype[key]).toBeDefined();
-//           });
-//         }
-//       });
-//     }
-//   });
-// });
+        if (properties.length > 0) {
+          test.each(properties)(`%s exists`, (key, descriptor) => {
+            expect(c.prototype[key]).toBeDefined();
+          });
+        }
+      });
+    }
+  });
+});
